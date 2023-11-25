9c69ec2e4e6405564efebcf5d38e6b60b81aba71
Jimmy Shan <jianyun.ui@gmail.com>
2023-11-25 13:17:45 -0800

    add script to test selenium

--- users/views.py
+++ users/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
 class RegisterView(View):
     def get(self, request):
         form = UserRegisterForm()
@@ -11,3 +15,16 @@ class RegisterView(View):
         if form.is_valid():
             form.save()
             return redirect('index')
+
+def test_chrome():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+    driver.get("https://www.google.com")
+    print(driver.service.path)
+    print(driver.title)
+    print("ok")
+
+    sleep(10)
+    driver.close()
